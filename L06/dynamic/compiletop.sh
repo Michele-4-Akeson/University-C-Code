51cc39f058dde510a1c3f717bdf9be85f719a8cc
Michele Akeson <akesonm@mcmaster.ca>
2021-07-15 10:13:16 -0400

    Making sure files are added corretly

--- L06/dynamic/compiletop.sh
+++ L06/dynamic/compiletop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ] ; then
-    echo "please provide the path to the shared library, and state its purpose"
+    echo "please provide the path to the shared library. This will allow the link to locate your library for compliation of your executable"
 else
     arg=$1
     gcc -L$arg -Wl,-rpath=$arg -Wall -o top top.c -lLab6Part5
