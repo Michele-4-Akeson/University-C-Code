adcfcde76c4258f34b9cb60e227fccffcb73c694
Michele Akeson <akesonm@mcmaster.ca>
2021-07-25 16:43:58 -0400

    commiting a terribly dreadful c script

---
+++ L08/part4/worst.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+  int array[] = {1, 2, 3};
+  bool x = true;
+  int n = array[3]/0;
+  
+  if (array[0] == 1) {
+    x++;
+    
+  } else if (array[0] == 1) {
+    x++;
+  }
+
+
+  return 0;
+}
