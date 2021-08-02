c73048afd218dea9f5dc0714563a5ac2aba481bd
Michele Akeson <akesonm@mcmaster.ca>
2021-08-02 11:08:38 -0400

    q3 seems to be working well - still need to figure out if zero division needs to be handled

--- A2/Q3/question3.c
+++ A2/Q3/question3.c
@@ -178,15 +178,15 @@ int simpleParse(const char *exp) {
 int main() {
     char exp1[] = "2 + 2";
     printf("%s = %d\n", exp1, simpleParse(exp1));
+        printf("Expected Value = 4\n");
     // Expected Value = 4
     char exp2[] = "3 + 7 - 4 * 1";
     printf("%s = %d\n", exp2, simpleParse(exp2));
-    // Expected Value = 6
+    printf("Expected Value = 6\n");
     char exp3[] = "9 * 13 / 2 + 18 * 110 - 812 \% 97 + 3";
     printf("%s = %d\n", exp3, simpleParse(exp3));
     printf("Expected %d\n ", 9 * 13 / 2 + 18 * 110 - 812 % 97 + 3);
-    printf("Expected %d\n", 2 + 110 - 812 % 97 + 3);
-    printf("812 97 %d", 812 % 97);
+  
     
     // Expected Value = 2005
     
